c5f70124dae532219d50bf6af122aa717832e4a6
Miroslaw-Keil <Miroslaw.Keil@Student.HTW-Berlin.de>
2022-11-03 17:18:30 +0100

    Test Klasse

---
+++ src/main/java/htw/berlin/webtech/Projekt/Test.java
@@ -0,0 +1,14 @@
+package htw.berlin.webtech.Projekt;
+
+import java.io.File;
+
+public class Test {
+
+    private Long id;
+    private String name;
+    private int zubereitungsdauer;
+    private boolean vegan;
+    private String tageszeit;
+
+    private File pdf;
+}
