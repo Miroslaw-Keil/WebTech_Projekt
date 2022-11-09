53c735c654cad56816eb388554cedc793087bbae
Miroslaw-Keil <Miroslaw.Keil@Student.HTW-Berlin.de>
2022-11-09 14:01:52 +0100

    Urls um "/" erweitert

--- src/main/java/htw/berlin/webtech/Projekt/web/TestRestController.java
+++ src/main/java/htw/berlin/webtech/Projekt/web/TestRestController.java
@@ -22,26 +22,26 @@ public class TestRestController {
     public ResponseEntity<List<Test>> fetchTests() {
        return ResponseEntity.ok(testService.findAll()); }
 
-    @GetMapping(path = "api/v1/tests/{id}")
+    @GetMapping(path = "/api/v1/tests/{id}")
     public ResponseEntity<Test> fetchTestById(@PathVariable Long id){
         var test = testService.findById(id);
         return test != null? ResponseEntity.ok(test) : ResponseEntity.notFound().build();
     }
 
-    @PostMapping(path = "api/v1/tests")
+    @PostMapping(path = "/api/v1/tests")
     public ResponseEntity<Void> createTest(@RequestBody TestCreateOrUpdateRequest request) throws URISyntaxException {
         var test = testService.create(request);
         URI uri = new URI("api/v1/tests" + test.getId());
         return ResponseEntity.created(uri).build();
     }
 
-    @PutMapping(path = "api/v1/tests/{id}")
+    @PutMapping(path = "/api/v1/tests/{id}")
     public ResponseEntity<Test> updateTest(@PathVariable Long id, @RequestBody TestCreateOrUpdateRequest request){
         var test = testService.update(id, request);
         return test != null? ResponseEntity.ok(test) : ResponseEntity.notFound().build();
     }
 
-    @DeleteMapping(path = "api/v1/tests/{id}")
+    @DeleteMapping(path = "/api/v1/tests/{id}")
     public ResponseEntity<Void> deleteTest(@PathVariable Long id) {
         boolean successful = testService.deleteById(id);
         return successful? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
